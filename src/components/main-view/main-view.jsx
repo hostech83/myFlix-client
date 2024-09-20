4c5c590c79c6eaee766dfde77f99ebf793104eb2
hostech83 <166859198+hostech83@users.noreply.github.com>
2024-09-20 01:30:55 -0400

    npm install react-router react-router-dom --save
    add BrowserRoutes, Routes, Route

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -7,6 +7,7 @@ import { SignupView } from "../signup-view/signup-view";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 export const MainView = () => {
   const storedUser = JSON.parse(localStorage.getItem("user"));
@@ -43,72 +44,81 @@ export const MainView = () => {
     setSelectedMovie(null);
   };
 
-  // If no user is logged in, show the login form
-  if (!user) {
-    return (
-      <Row className="justify-content-md-center">
-        <Col md={12} className="text-center my-3">
-          <h1>MyflixApp</h1>
-        </Col>
-        <Col md={5}>
-          <LoginView
-            onLoggedIn={(user, token) => {
-              setUser(user);
-              setToken(token);
-              // Store user and token in localStorage
-              localStorage.setItem("user", JSON.stringify(user));
-              localStorage.setItem("token", token);
-            }}
-          />
-        </Col>
-        or
-        <Col md={5}>
-          {/* Signup Form */}
-          <SignupView />
-        </Col>
-      </Row>
-    );
-  }
-
   // If a movie is selected, show the movie details
   if (selectedMovie) {
     return <MovieView movie={selectedMovie} onBackClick={onBackClick} />;
   }
 
   return (
-    <div>
-      {/* Logout Button */}
-      <button
-        variant="danger"
-        onClick={() => {
-          setUser(null);
-          setToken(null);
-          localStorage.clear(); // Clear localStorage on logout
-        }}
-      >
-        Logout
-      </button>
-
-      {/* Show the list of movies */}
-      <Row>
-        {movies.map((movie) => (
-          <Col className="mb-5" key={movie._id} md={3}>
-            {/* Use _id as the key */}
-            <MovieCard movie={movie} onMovieClick={onMovieClick} />
-          </Col>
-        ))}
+    <BrowserRouter>
+      <Row className="justify-content-md-center">
+        <Routes>
+          <Route
+            path="/signup"
+            element={
+              <>
+                {user ? (
+                  <Navigate to="/" />
+                ) : (
+                  <Col md={5}>
+                    <SignupView />
+                  </Col>
+                )}
+              </>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <>
+                {user ? (
+                  <Navigate to="/" />
+                ) : (
+                  <Col md={5}>
+                    <LoginView onLoggedIn={(user) => setUser(user)} />
+                  </Col>
+                )}
+              </>
+            }
+          />
+          <Route
+            path="/movies/:movieId"
+            element={
+              <>
+                {!user ? (
+                  <Navigate to="/login" replace />
+                ) : books.length === 0 ? (
+                  <Col>The list is empty!</Col>
+                ) : (
+                  <Col md={8}>
+                    <movieView movies={movies} />
+                  </Col>
+                )}
+              </>
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <>
+                {!user ? (
+                  <Navigate to="/login" replace />
+                ) : movies.length === 0 ? (
+                  <Col>The list is empty!</Col>
+                ) : (
+                  <>
+                    {movies.map((movie) => (
+                      <Col className="mb-4" key={movie.id} md={3}>
+                        <BookCard movie={movie} />
+                      </Col>
+                    ))}
+                  </>
+                )}
+              </>
+            }
+          />
+        </Routes>
       </Row>
-    </div>
+    </BrowserRouter>
   );
 };
-// PropTypes for MainView if needed (depends on whether you expect props to be passed to MainView)
-//MainView.propTypes = {
-// movies: PropTypes.arrayOf(
-// PropTypes.shape({
-//_id: PropTypes.string.isRequired,
-// Title: PropTypes.string.isRequired,
-// Description: PropTypes.string.isRequired,
-// })
-// ).isRequired,
-// onMovieClick: PropTypes.func.isRequired,
-//};
