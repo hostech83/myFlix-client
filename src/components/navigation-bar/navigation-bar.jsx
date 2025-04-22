dbee8dcad86ec3516cd3b3a0c53cb99bc39bc7cd
hostech83 <166859198+hostech83@users.noreply.github.com>
2025-04-22 19:21:22 -0400

    refactor the styling

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -9,61 +9,64 @@ export const NavigationBar = ({ user, onLogout }) => {
   return (
     <>
       <Navbar
-        bg="dark"
         expand="lg"
         expanded={expanded}
         className="navigation-bar"
+        sticky="top"
       >
-        <Navbar.Brand as={Link} to="/" onClick={() => setExpanded(false)}>
-          MyFlix App
-        </Navbar.Brand>
-        <Navbar.Toggle
-          aria-controls="navbar-nav"
-          onClick={() => setExpanded(expanded ? false : "expanded")}
-        />
-        <Navbar.Collapse id="navbar-nav">
-          <Nav className="me-auto">
-            {!user ? (
-              <>
-                <Nav.Link
-                  as={Link}
-                  to="/login"
-                  onClick={() => setExpanded(false)}
-                >
-                  Login
-                </Nav.Link>
-                <Nav.Link
-                  as={Link}
-                  to="/signup"
-                  onClick={() => setExpanded(false)}
-                >
-                  Signup
-                </Nav.Link>
-              </>
-            ) : (
-              <>
-                <Nav.Link as={Link} to="/" onClick={() => setExpanded(false)}>
-                  Home
-                </Nav.Link>
-                <Nav.Link
-                  as={Link}
-                  to="/profile"
-                  onClick={() => setExpanded(false)}
-                >
-                  Profile
-                </Nav.Link>
-                <Nav.Link
-                  onClick={() => {
-                    onLogout();
-                    setExpanded(false);
-                  }}
-                >
-                  Logout
-                </Nav.Link>
-              </>
-            )}
-          </Nav>
-        </Navbar.Collapse>
+        {" "}
+        <Container fluid>
+          <Navbar.Brand as={Link} to="/" onClick={() => setExpanded(false)}>
+            MyFlix App
+          </Navbar.Brand>
+          <Navbar.Toggle
+            aria-controls="navbar-nav"
+            onClick={() => setExpanded(expanded ? false : "expanded")}
+          />
+          <Navbar.Collapse id="navbar-nav">
+            <Nav className="me-auto">
+              {!user ? (
+                <>
+                  <Nav.Link
+                    as={Link}
+                    to="/login"
+                    onClick={() => setExpanded(false)}
+                  >
+                    Login
+                  </Nav.Link>
+                  <Nav.Link
+                    as={Link}
+                    to="/signup"
+                    onClick={() => setExpanded(false)}
+                  >
+                    Signup
+                  </Nav.Link>
+                </>
+              ) : (
+                <>
+                  <Nav.Link as={Link} to="/" onClick={() => setExpanded(false)}>
+                    Home
+                  </Nav.Link>
+                  <Nav.Link
+                    as={Link}
+                    to="/profile"
+                    onClick={() => setExpanded(false)}
+                  >
+                    Profile
+                  </Nav.Link>
+                  <Nav.Link
+                    onClick={() => {
+                      onLogout();
+                      setExpanded(false);
+                    }}
+                  >
+                    Logout
+                  </Nav.Link>
+                </>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
       </Navbar>
       <div style={{ marginTop: "2px" }}>
         {" "}
